09550c741d615e1e0437ec9f6fe1495f8f4fe30f
angellane <fabianferreira36@gmail.com>
2026-03-04 19:05:39 +0000

    Server Structure + IncorrectActionException

--- Server/24403148_24432024_Server/src/main/java/com/mycompany/_24432024_server/IncorrectActionException.java
+++ Server/24403148_24432024_Server/src/main/java/com/mycompany/_24432024_server/IncorrectActionException.java
@@ -1,16 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany._24432024_server;
 
 /**
  *
  * @author fabia
  */
-public class App {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!"); //test for github
-    }
+public class IncorrectActionException {
+    
 }
---
+++ Server/24403148_24432024_Server/src/main/java/com/mycompany/_24432024_server/ServerMain.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany._24432024_server;
+
+/**
+ *
+ * @author fabia
+ */
+public class ServerMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); //test for github
+    }
+}
