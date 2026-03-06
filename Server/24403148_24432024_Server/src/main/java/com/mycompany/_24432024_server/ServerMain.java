dfbc888e5cc2f3c5115e65a416c43ac7e81e65cd
angellane <fabianferreira36@gmail.com>
2026-03-06 20:41:04 +0000

    Update ServerMain.java

--- Server/24403148_24432024_Server/src/main/java/com/mycompany/_24432024_server/ServerMain.java
+++ Server/24403148_24432024_Server/src/main/java/com/mycompany/_24432024_server/ServerMain.java
@@ -1,9 +1,10 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
+
+package com.mycompany._24432024_server;
 import java.net.*;
 import java.io.*;
-package com.mycompany._24432024_server;
 
 /**
  *
@@ -12,8 +13,21 @@ package com.mycompany._24432024_server;
 public class ServerMain {
     public static void main(String[] args) {
         try (ServerSocket servSock = new ServerSocket(1234)) {
+            System.out.println("Server Started");
             
-        }
+           while (true) {
+               Socket link = servSock.accept();
+               BufferedReader in = new BufferedReader(new InputStreamReader(link.getInputStream()));
+               PrintWriter out = new PrintWriter(link.getOutputStream(), true);
+               
+               out.println("Awaiting data.");
+               String input = in.readLine();
+               System.out.println("Received: " + input);
+                       
+               link.close();
+           }
+        } catch (IOException e) {
+            e.printStackTrace();
     }
-  
+    }
 }
